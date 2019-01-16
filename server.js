4279968da9c4e837cdf458256fda352be807eab7
phamin2001 <amin.pahlavani@gmail.com>
2019-01-16 06:07:09 -0700

    Commit 2 - Connected my database, can see json in the browser

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express  = require('express');
 const app      = express();
+const Pokemon = require('./pokemon');
 
+app.get('/pokemon', (req, res) => {
+    res.send(Pokemon);
+    // res.render('index.ejs', {
+    //     pokemon: Pokemon
+    // })
+});
 
 app.listen(3000, () => {
     console.log('Server is listening on port 3000');
