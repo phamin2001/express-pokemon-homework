07349dac9aeba2303830554172bfd57004550ab7
phamin2001 <amin.pahlavani@gmail.com>
2019-01-16 06:41:58 -0700

    Commit 3 - index.ejs view rendered at pokemon route

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express  = require('express');
 const app      = express();
 const Pokemon = require('./pokemon');
+app.use(express.static(__dirname + '/public'));
 
+
+// index route
 app.get('/pokemon', (req, res) => {
-    res.send(Pokemon);
-    // res.render('index.ejs', {
-    //     pokemon: Pokemon
-    // })
+    // res.send(Pokemon);
+
+    res.render('index.ejs', {
+        pokemon: Pokemon
+    })
 });
 
 app.listen(3000, () => {
